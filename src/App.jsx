33a78291c0f5db44797be4edd03de283e7577488
Nouran AlSharawneh <noransharawna@hotmail.com>
2025-07-06 14:59:54 +0300

    bg updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TodoPage from "./pages/TodoPage";
 
 function App() {
   return (
-    <main className="bg-white">
+    <main className="min-h-screen bg-white bg-[radial-gradient(#d1d5db_1px,_transparent_1px)] [background-size:20px_20px]">
       <TodoPage />
     </main>
   );
