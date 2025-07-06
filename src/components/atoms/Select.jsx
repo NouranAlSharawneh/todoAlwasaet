51554d8ca9339eab0201b383f99cd63592dbeeb7
Nouran AlSharawneh <noransharawna@hotmail.com>
2025-07-06 15:24:42 +0300

    updating the select height

--- src/components/atoms/Select.jsx
+++ src/components/atoms/Select.jsx
@@ -1,7 +1,7 @@
 function Select({ options, value, onChange, className }) {
   return (
     <select
-      className={`border px-2 py-2 rounded bg-white text-xs md:text-sm text-gray-800 border-gray-300 cursor-pointer focus:outline-none focus:ring-1 focus:ring-blue-500 ${className}`}
+      className={`h-8.5 border px-2 py-2 rounded bg-white text-xs md:text-sm text-gray-800 border-gray-300 cursor-pointer focus:outline-none focus:ring-1 focus:ring-blue-500 ${className}`}
       value={value}
       onChange={(e) => onChange(e.target.value)}
     >
