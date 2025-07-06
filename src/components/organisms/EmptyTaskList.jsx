2cadf3924005dad1417b87119f8de5e1a5125e0d
Nouran AlSharawneh <noransharawna@hotmail.com>
2025-07-06 15:25:21 +0300

    update background color in EmptyTaskList component

--- src/components/organisms/EmptyTaskList.jsx
+++ src/components/organisms/EmptyTaskList.jsx
@@ -1,6 +1,6 @@
 export default function EmptyTaskList() {
   return (
-    <div className="flex flex-col items-center justify-center text-center py-10 text-gray-500 h-[40vh] bg-gray-100 rounded-lg shadow-md mt-10">
+    <div className="flex flex-col items-center justify-center text-center py-10 text-gray-500 h-[40vh] bg-gray-50 rounded-lg shadow-md mt-10">
       <div className="text-4xl mb-2">📝</div>
       <h2 className="text-base md:text-xl font-semibold">No tasks yet</h2>
       <p className="text-sm">start adding a new task using the above button</p>
