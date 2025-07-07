086856b518c26f063435366e82a94d5c107cf520
Nouran AlSharawneh <noransharawna@hotmail.com>
2025-07-07 13:03:25 +0300

    Added animation

--- src/components/organisms/TaskList.jsx
+++ src/components/organisms/TaskList.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import TaskItem from "../molecules/TaskItem";
 import EmptyTaskList from "./EmptyTaskList";
 
@@ -8,9 +10,20 @@ function TaskList({ tasks, setTasks }) {
       {tasks.length === 0 ? (
         <EmptyTaskList />
       ) : (
-        tasks.map((task) => (
-          <TaskItem key={task.id} task={task} setTasks={setTasks} />
-        ))
+        <AnimatePresence>
+          {tasks.map((task) => (
+            <motion.div
+              key={task.id}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.75, ease: "easeInOut" }}
+              layout
+            >
+              <TaskItem task={task} setTasks={setTasks} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
       )}
     </div>
   );
