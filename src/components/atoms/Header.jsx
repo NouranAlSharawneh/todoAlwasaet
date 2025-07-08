ff383107c04c7e0e91b78100ce99d9c0ed07ce65
Nouran AlSharawneh <noransharawna@hotmail.com>
2025-07-08 11:36:35 +0300

    updated img aspect ratio

--- src/components/atoms/Header.jsx
+++ src/components/atoms/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
       <img
         src="assets/listIcon.png"
         alt="Todo Alwasaet Logo"
-        className="md:w-40 md:h-40 w-20 h-20 cursor-pointer"
+        className="md:w-40 md:h-full w-20 h-full cursor-pointer hover:scale-105 hover:rotate-10 transition-transform duration-300"
       />
       <h1 className="text-5xl md:text-8xl text-center">Todo List</h1>
     </div>
